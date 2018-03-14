c8075a42f83573ae330f0acc23891e6d320b17b7
nicolas <nicolas.dejean@epitech.eu>
2018-03-14 17:00:04 +0100

    started mapactivity

--- App/app/src/main/java/com/example/tetard/streetlive/AuthentificationActivity.java
+++ App/app/src/main/java/com/example/tetard/streetlive/AuthentificationActivity.java
@@ -1,5 +1,6 @@
 package com.example.tetard.streetlive;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- App/app/src/main/java/com/example/tetard/streetlive/MapActivity.java
+++ App/app/src/main/java/com/example/tetard/streetlive/MapActivity.java
@@ -1,7 +1,10 @@
 package com.example.tetard.streetlive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.mapToolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_tab);
+
     }
 }
