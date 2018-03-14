58c020d3c1971f09b6df057fd3926ec32b031a4e
Benjamin Girard <benjamin.girard@epitech.eu>
2018-03-14 14:33:10 +0100

    added split function for social connection + test for db

--- App/app/src/main/java/com/example/tetard/streetlive/AuthentificationActivity.java
+++ App/app/src/main/java/com/example/tetard/streetlive/AuthentificationActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.tetard.streetlive.Database.DatabaseHandler;
@@ -28,6 +29,13 @@ import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static java.sql.DriverManager.getConnection;
+
 public class AuthentificationActivity extends AppCompatActivity {
 
     DatabaseHandler db = new DatabaseHandler();
@@ -50,15 +58,16 @@ public class AuthentificationActivity extends AppCompatActivity {
     String              _token;
     String              _secret;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private void FBConnection() {
         /*
         *  Facebook initialization (app ID and secret in manifests/AndroidManifest.xml)
         * */
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         callbackManager = CallbackManager.Factory.create();
+    }
+
+    private void twitterConnectionInit() {
         /*
         *  Twitter initialization
         */
@@ -69,19 +78,40 @@ public class AuthentificationActivity extends AppCompatActivity {
                 .debug(true)
                 .build();
         Twitter.initialize(config);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FBConnection();
+        twitterConnectionInit();
 
         setContentView(R.layout.activity_authentification);
-        Log.wtf("ONCREATE", "entered create");
-        Log.wtf("CREATE", "quit create");
 
-        db.connect("http://127.0.0.1:3306/phpmyadmin/index.php?db=test&table=category&target=sql.php&token=7ba13a3a742ca58c4a4e2c4418f99134#PMAURL:db=test&table=category&target=sql.php&token=7ba13a3a742ca58c4a4e2c4418f99134");
         twitterLoginCallback();
         mTwitterAuthClient = new TwitterAuthClient();
+
     }
     public void connectButton(View view) {
+        EditText username = (EditText) findViewById(R.id.name_enter);
+        Log.wtf("username", username.getText().toString());
+
+        try {
+            Connection dbConnection = getConnection("localhost:3306", "root", "");
+            Statement statement = dbConnection.createStatement();
+            ResultSet rs = statement.executeQuery("INSERT INTO user (info, profile, type, logs, bonus) VALUES (\"toto\", \"titi\", \"toto\", \"titi\", \"toto\")");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.wtf("SQL FAIL", "connectButton: " + e.getMessage());
+        }
+
+
         Intent intent = new Intent(this, ArtistLocate.class);
         startActivity(intent);
     }
+
 
     public void signupButton(View view) {
         Intent intent = new Intent(this, CreateUserActivity.class);
@@ -89,7 +119,7 @@ public class AuthentificationActivity extends AppCompatActivity {
     }
 
     public void facebookLoginCallBack() {
-        _fbLoginButton = (LoginButton) findViewById(R.id.facebook_connect);
+        _fbLoginButton = findViewById(R.id.facebook_connect);
         _fbLoginButton.setReadPermissions("email");
         // Callback registration
         _fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -119,7 +149,6 @@ public class AuthentificationActivity extends AppCompatActivity {
         _twLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                Log.wtf("SUCESS", "entered success");
                 TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
                 TwitterAuthToken authToken = session.getAuthToken();
                 _token = authToken.token;
